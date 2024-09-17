a628a3ca63b54f1c797d7eb8f8c6156af3073350
jagadeesh <jagadeeshakula123@gmail.com>
2024-09-17 20:12:03 +0530

    terraform

--- count/ec2.tf
+++ count/ec2.tf
@@ -15,16 +15,20 @@ resource "aws_security_group" "allow_ssh_terraform" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-  tags = {
-    Name = "allow_ssh"
-  }
+  tags = merge(
+    var.common_tags, {
+      Name = "allow_ssh"
+    }
+  )
 }
 resource "aws_instance" "terraform" {
-  count                  = 3
+  count                  = length(var.instance_names)
   ami                    = "ami-09c813fb71547fc4f"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-  tags = {
-    Name = var.instance_names[count.index]
-  }
+  tags = merge(
+    var.common_tags, {
+      Name = var.instance_names[count.index]
+    }
+  )
 }
--- count/variables.tf
+++ count/variables.tf
@@ -2,3 +2,11 @@ variable "instance_names" {
   default = ["database", "backend", "frontend"]
   type    = list(string)
 }
+variable "common_tags" {
+  default = {
+    Project     = "expense"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+  type = map(any)
+}
