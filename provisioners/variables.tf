7ca165f5ee2b24c57e0bbe5a79ec4425e476313f
jagadeesh <jagadeeshakula123@gmail.com>
2024-09-19 19:42:43 +0530

    terraform

---
+++ provisioners/variables.tf
@@ -0,0 +1,18 @@
+variable "ingress_rules" {
+  default = [
+    {
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    },
+    {
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+}
