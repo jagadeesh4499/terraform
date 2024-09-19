39d362aacf3021f1d8db910a3181f824effb8920
jagadeesh <jagadeeshakula123@gmail.com>
2024-09-19 18:14:48 +0530

    terraform

---
+++ dynamic/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+  name        = "allow ssh"
+  description = "Allow port number 22 for SSH acess"
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      description = ingress.value.description
+    }
+  }
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+resource "aws_instance" "terraform" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+  tags = {
+    Name = "terraform"
+  }
+}
