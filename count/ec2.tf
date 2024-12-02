1601ae812ba5e1dddc11e70610af8cc99facb3a2
jagadeesh <jagadeeshakula123@gmail.com>
2024-12-02 22:24:46 +0530

    count

--- count/ec2.tf
+++ count/ec2.tf
@@ -3,9 +3,12 @@ resource "aws_instance" "terraform" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-    tags = {
-        Name = var.instance_names[count.index]
-    }
+    tags = merge(
+        var.common_tags,
+        {
+            Name = var.instance_names[count.index]
+        }
+    )
 }
 resource "aws_security_group" "allow_ssh"{
     name    = "allow ssh"
--- count/variables.tf
+++ count/variables.tf
@@ -2,3 +2,10 @@ variable "instance_names" {
     default = ["mysql", "backend", "frontend"]
     type = list(string)
 }
+variable "common_tags" {
+    default = {
+        Project = "Expense"
+        Environment = "Dev"
+        Terraform = true
+    }
+}
